2f84a79191f3282427d5c57f4ada37e916965a97
pablobatista00 <89108228+pablobatista00@users.noreply.github.com>
2021-10-25 20:50:37 -0300

    Add files via upload
    aula 04 exercicios

---
+++ aula4.js
@@ -0,0 +1,33 @@
+console.log("Ola Mundo!")
+
+var x = 8; //atribuicao
+const y = 6;
+let z = 2;
+
+let soma = x + y +z;
+let subtracao = x - y - z;
+let divisao = x/y;
+let multiplicacao = x*y;
+
+console.log(soma);
+console.log(subtracao);
+console.log(divisao);
+console.log(multiplicacao); 
+
+let nome = "Pablo";
+let idade = 25;
+
+console.log(nome+" - "+idade);
+
+z = z+1
+console.log(z)
+
+let nomes = "Aline"
+let idades = 33
+let peso = 63.7
+let Altura = 1.53
+let Plano = "Nao tem"
+let Indice = peso/(Altura*Altura)
+
+console.log(nomes+ " tem "+ idades+" anos e seu imc é de "+Indice)
+
